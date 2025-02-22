d71be3d132732d264074a162191d31988feaf808
croissen <croissen214@gmail.com>
2025-02-22 22:38:36 +0900

    java day11 practice

---
+++ practiceTMS/src/practiceTMS/Client.java
@@ -0,0 +1,25 @@
+package practiceTMS;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) throws IOException{
+		Socket clientSocket = null;
+		PrintWriter writer = null;
+		BufferedReader in = null;
+		BufferedWriter out = null;
+		Scanner sc = new Scanner(System.in);
+		
+		String serverIp = "localhost";
+		
+		
+		
+		
+		
+	}
+}
--- practiceTMS/src/practiceTMS/Server.java
+++ practiceTMS/src/practiceTMS/Server.java
@@ -1,4 +1,4 @@
-package chat02;
+package practiceTMS;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,7 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Server02 {
+public class Server {
 	public static void main(String[] args) throws IOException{
 		ServerSocket serverSocket = null;
 		Socket socket = null;
@@ -17,34 +17,34 @@ public class Server02 {
 		BufferedWriter out = null;
 		Scanner sc = new Scanner(System.in);
 		
-		
 		try {
-//			서버소켓 생성
 			serverSocket = new ServerSocket(7777);
 			System.out.println("연결 대기중...");
 			
 			socket = serverSocket.accept();
-			System.out.println("연결되었습니다.");
+			System.out.println("연결 완료");
 			
-//			네트워크 입출력 스트림 생셩
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			
 			while(true) {
 				String inMessage = in.readLine();
-				System.out.println("클라이언트 > " + inMessage);
-			
-			
-//				클라이언트로 데이터 전송
+				System.out.println("클라이언트 >" + inMessage);
+	
+				
 				System.out.println("서버에서 클라이언트로 보내기 >>");
 				String outMessage = sc.nextLine();
 				out.write(outMessage + "\n");
 				out.flush();
+				
 			}
+			
+			
+			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			if(sc != null) {
 				sc.close();
 			}
@@ -54,22 +54,13 @@ public class Server02 {
 			if(socket != null) {
 				socket.close();
 			}
-			if( serverSocket != null) {
+			if(serverSocket != null) {
 				serverSocket.close();
 			}
 		}
 		
 		
+		
+		
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
